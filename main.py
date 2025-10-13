8a1e358719e589aded5285edb1fb0ae62e32758f
arifiqbal01 <153394683+arifiqbal01@users.noreply.github.com>
2025-10-13 08:42:25 +0000

    Learning python syntax: variable and data type

--- main.py
+++ main.py
@@ -1,32 +1,14 @@
-import random
+# variables
+name = "Beau" # whole line is called statement
+age = 25
+Fruits = ["banana", "pine", "apple"]
+print(type(age))
 
-def get_choices():
-  player_choice = input("Enter a choice (rock, paper, scissors: ")
-  options = ["rock", "paper", "scissors"]
-  computer_choice = random.choice(options)
-  choices = {"player": player_choice, "computer": computer_choice}
-  return choices
+# data type 
+age = 30 
+print(isinstance(age, int))
 
-def check_win(player, computer):
-  print(f"player chosed {player} computer chosed {computer}")
-  if player == computer:
-    return ("it's a tie!")
-  elif player == "rock": 
-    if computer == "scissors":
-      return ("rock smashes scissors. You win")
-    else:
-      return ("Paper cover rock. you lose")
-  elif player == "paper":
-    if computer == "rock":
-      return ("paper covers rock. You win")
-    else:
-      return ("Scissors cut paper. you lose")
-  elif player == "scissors":
-    if computer == "paper":
-      return ("Scissors cut paper. You lose")
-    else:
-      return ("Rock smashes scissors. You win")
-    
-choices = get_choices()
-result = check_win(choices["player"], choices["computer"])
-print(result)
+age = str(2.5) 
+print(isinstance(age, str))
+
+
