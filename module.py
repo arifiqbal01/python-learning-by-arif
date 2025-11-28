c3d72267cf62d9ba4b18fa783adef6ec33d631d3
Arif Iqbal <arifghaleja0@gmail.com>
2025-11-28 07:51:49 +0000

    Learning python syntax: modules, libraries, passing arguments

---
+++ module.py
@@ -0,0 +1,13 @@
+import sys
+import argparse
+from math import sqrt
+from lib import dog
+
+dog.bark()
+
+# Libraries
+print(sqrt(4))
+
+# accept arguments 
+name = sys.argv[2]
+print("hello " + name)
